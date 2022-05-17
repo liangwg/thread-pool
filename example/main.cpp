1916c9314001b973fa2015f6b3c6a303c817854e
liangwg <2870875593@qq.com>
2022-05-17 21:49:53 +0800

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -44,6 +44,8 @@ int main(int argc, char *argv[])
   // Initialize pool
   pool.init();
 
+  
+  //这里提交2*9=18次任务。
   // Submit (partial) multiplication table
   for (int i = 1; i < 3; ++i) {
     for (int j = 1; j < 10; ++j) {
